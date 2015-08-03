e98e02396876db78b899eb23271c8f7cb6d7c293
unknown <ddatta@NLWS483532.upcit.ds.upc.biz>
2015-08-03 02:18:35 +0200

    First commit with Catman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name catman10App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the catman10App
+ */
+angular.module('catman10App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name catman10App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the catman10App
+ */
+angular.module('catman10App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
